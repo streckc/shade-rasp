9e3f3f8b494b28f32bb006e6d4ebcdbafc5c351c
Chad Streck <cstreck@tenable.com>
2019-02-18 00:33:06 -0600

    load missing df sensor

--- sensor/sensors/__init__.py
+++ sensor/sensors/__init__.py
@@ -4,6 +4,7 @@ from .sensor_arp_scan import arp_scan_sensor
 from .sensor_avahi_browse import avahi_browse_sensor
 from .sensor_nmap import nmap_sensor
 from .sensor_vmstat import vmstat_sensor
+from .sensor_vmstat import df_sensor
 
 sensor_list = {
     'arp-scan': arp_scan_sensor,
